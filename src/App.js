e7c462be7f409f426aca971142caabd0fe397007
dima <dmitriy.dubchenko@gmail.com>
2019-04-04 20:13:28 +0200

    Innitial commit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
           </a>
-        </header>
-      </div>
-    );
-  }
+				</header>
+				<div className="shopping-list">
+					<h1>Shopping List for {this.props.name}</h1>
+					<ul>
+						<li>Instagram</li>
+						<li>WhatsApp</li>
+						<li>Oculus</li>
+					</ul>
+				</div>
+			</div>
+
+		);
+	}
 }
 
 export default App;
+
+
